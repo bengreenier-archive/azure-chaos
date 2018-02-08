9a0d5343dafb28a56dbc4fd9efb279e3149e0a9a
Ben Greenier <begreen@microsoft.com>
2018-02-08 12:28:33 -0800

    v1.1.1 - updated module exports to include missing commands

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ module.exports = {
     RequestProcessor: require('./lib/request-processor'),
     factory: require('./lib/factory'),
     register: require('./commands/register').handler,
+    list: require('./commands/list').handler,
     unregister: require('./commands/unregister').handler,
     start: require('./commands/start').handler,
     stop: require('./commands/stop').handler,
-    resgen: require('./commands/resgen').handler
+    resgen: require('./commands/resgen').handler,
+    token: require('./commands/token').handler
 }
